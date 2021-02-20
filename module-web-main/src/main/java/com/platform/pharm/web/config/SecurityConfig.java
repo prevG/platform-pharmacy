78873798989998aa519498ff012689328222fb70
JooYongjin <JooYongjin@desktop.mshome.net>
2021-02-20 16:18:02 +0900

    123

--- module-web-main/src/main/java/com/platform/pharm/web/config/SecurityConfig.java
+++ module-web-main/src/main/java/com/platform/pharm/web/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers("/js/**", "/css/**");	
 	}
 	
-	@Override
+	@Override 
 	protected void configure(HttpSecurity http) throws Exception {
         http
 			.csrf()
